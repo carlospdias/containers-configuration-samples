aa3eaac45184fd2369322d6cf535bc86c3909381
Carlos Pereira Dias <carlospdiase@gmail.com>
2025-04-06 15:39:35 -0300

    Ajusta 'scripts' de infra

---
+++ postgresql/clean-docker-env.sh
@@ -0,0 +1,4 @@
+yes Y | docker system prune -a
+yes Y | docker image prune
+yes Y | docker volume prune 
+yes Y | docker network prune
--- postgresql/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE SEQUENCE book_sq INCREMENT 1 MINVALUE 1  START 1 CACHE 1;
-
-CREATE TABLE book(
-    id bigint NOT NULL,  
-    publishingdate date, 
-    title character varying(255), 
-    version integer, 
-    CONSTRAINT book_pkey PRIMARY KEY (id)
-);
