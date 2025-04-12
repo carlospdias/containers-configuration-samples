9618ed01213eb914d6d9d060df9c5d8c3ea4477a
Carlos Pereira Dias <carlospdiase@gmail.com>
2025-04-11 21:40:13 -0300

    :tada: Adiciona configuração

---
+++ postgresql-with-pgadmin/clean-docker-env.sh
@@ -0,0 +1,5 @@
+yes Y | docker system prune -a
+yes Y | docker image prune
+yes Y | docker volume prune 
+yes Y | docker network prune
+docker volume rm $(docker volume ls -q --filter dangling=true)
